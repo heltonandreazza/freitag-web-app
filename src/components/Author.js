ea75e0596e11af30dfd840cba8a929614c00e4c8
Helton Andreazza <heltonandreazza@Heltons-MacBook-Air.local>
2021-08-11 19:22:07 -0300

    fix author

--- src/components/Author.js
+++ src/components/Author.js
@@ -46,30 +46,6 @@ const Author = ({ author }) => (
               className="absolute inset-0 h-full w-full object-cover"
               image={author.image.gatsbyImageData}
             />
-            <div className="relative px-8 pt-64 pb-8">
-              <blockquote className="mt-8">
-                <div className="relative text-lg font-medium text-white md:flex-grow">
-                  <svg
-                    className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-blue-400"
-                    fill="currentColor"
-                    viewBox="0 0 32 32"
-                    aria-hidden="true"
-                  >
-                    <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-                  </svg>
-                  <p className="relative">{author.frase}</p>
-                </div>
-
-                <footer className="mt-4">
-                  <p className="text-base font-semibold text-blue-200">
-                    {author.name}
-                    ,{author.role}
-                    {' '}
-                    no Hospital de Olhos Freitag
-</p>
-                </footer>
-              </blockquote>
-            </div>
           </div>
         </div>
       </div>
@@ -115,9 +91,7 @@ const Author = ({ author }) => (
           </dl>
           <div className="mt-10">
             <a href="/authors" className="text-base font-medium text-blue-400 hover:text-blue-500">
-              Conheça a nossa equipe 
-{' '}
-<span aria-hidden="true">&rarr;</span>
+              Conheça a nossa equipe <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </div>
