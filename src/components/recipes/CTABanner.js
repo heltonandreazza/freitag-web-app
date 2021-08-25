fb2f48f375c8899fe34527256edd08733f247ae3
Helton Andreazza <heltonandreazza@Heltons-MacBook-Air.local>
2021-08-25 13:43:24 -0300

    change CTABanner

--- src/components/recipes/CTABanner.js
+++ src/components/recipes/CTABanner.js
@@ -7,11 +7,11 @@ import Button from '../Button'
 const CTABanner = () => (
   <div className='flex py-8 bg-blue-50 '>
     <div className='hidden sm:flex flex-1 px-4 justify-end items-center'>
-      <ClockIcon className='h-40 w-40 text-blue-400' />
+      <ClockIcon className='h-40 w-40 text-blue-500' />
     </div>
     <div className='flex-1 px-4 ng-blue-400'>
       <h2 className='text-center sm:text-left text-2xl font-extrabold tracking-tight text-gray-900'>
-        <span className='block text-blue-400 mb-2 text-3xl'>
+        <span className='block text-blue-500 mb-2 text-3xl'>
           Consultas e Exames
         </span>
         <a
@@ -21,7 +21,9 @@ const CTABanner = () => (
           )}
           target='_blank'
         >
-          <span className='block mb-2'>(47) 99262-6100</span>
+          <span className='underline text-blue-400 hover:text-blue-500 block mb-2'>
+            Timbó / Benedito Novo - (47) 99262-6100
+          </span>
         </a>
         <a
           href={getWhatsUrl(
@@ -30,7 +32,9 @@ const CTABanner = () => (
           )}
           target='_blank'
         >
-          <span className='block mb-2'>(47) 99168-9605</span>
+          <span className='underline text-blue-400 hover:text-blue-500 block mb-2'>
+            Indaial - (47) 99168-9605
+          </span>
         </a>
         <a
           href={getWhatsUrl(
@@ -39,7 +43,9 @@ const CTABanner = () => (
           )}
           target='_blank'
         >
-          <span className='block mb-2'>(47) 99175-2857</span>
+          <span className='underline text-blue-400 hover:text-blue-500 block mb-2'>
+            Apiúna - (47) 99175-2857
+          </span>
         </a>
       </h2>
     </div>
